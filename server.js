aef5f552b3a1f7a7d37a28466b84e14b1cf1c58a
jokandro <114486039+jokandro@users.noreply.github.com>
2023-10-08 13:41:51 +0800

    Added Backend

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://jamesbariuad:m8mD4Dx7nG0kbyId@todoapp.o705ngd.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define Todo model and schema using Mongoose
+const todoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Define routes for CRUD operations
+app.post('/api/todos', async (req, res) => {
+  try {
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({ error: 'Error creating Todo' });
+  }
+});
+
+// Get all Todos
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: 'Error fetching Todos' });
+  }
+});
+
+// Update a Todo
+app.put('/api/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ error: 'Todo not found' });
+
+    todo.title = req.body.title || todo.title;
+    todo.completed = req.body.completed || todo.completedg;
+
+    await todo.save();
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({ error: 'Error updating Todo' });
+  }
+});
+
+// Delete a Todo
+app.delete('/api/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.status(404).json({ error: 'Todo not found' });
+
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ error: 'Error deleting Todo' });
+  }
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
